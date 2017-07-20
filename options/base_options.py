876fa1032d980d3744374424d7c4af695de344ae
Audrey Lin <aauddrey@gmail.com>
2017-07-19 19:33:41 -0700

    modify to match tinyenv

--- options/base_options.py
+++ options/base_options.py
@@ -2,6 +2,9 @@ import argparse
 import os
 from util import util
 import torch
+from tinyenv import flags
+
+FLAGS = flags()
 
 class BaseOptions():
     def __init__(self):
@@ -9,7 +12,7 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self):
-        self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
+        self.parser.add_argument('--dataroot', default=FLAGS.input_dir + '/horse2zebra', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
         self.parser.add_argument('--loadSize', type=int, default=286, help='scale images to this size')
         self.parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
@@ -54,7 +57,7 @@ class BaseOptions():
             id = int(str_id)
             if id >= 0:
                 self.opt.gpu_ids.append(id)
-        
+
         # set gpu ids
         if len(self.opt.gpu_ids) > 0:
             torch.cuda.set_device(self.opt.gpu_ids[0])
--- train.py
+++ train.py
@@ -2,7 +2,13 @@ import time
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
-from util.visualizer import Visualizer
+from zipfile import ZipFile
+from tinyenv import flags
+
+FLAGS = flags()
+
+z = ZipFile(FLAGS.input_dir + '/horse2zebra.zip')
+z.extractall(FLAGS.input_dir)
 
 opt = TrainOptions().parse()
 data_loader = CreateDataLoader(opt)
@@ -11,7 +17,6 @@ dataset_size = len(data_loader)
 print('#training images = %d' % dataset_size)
 
 model = create_model(opt)
-visualizer = Visualizer(opt)
 
 total_steps = 0
 
@@ -24,15 +29,12 @@ for epoch in range(1, opt.niter + opt.niter_decay + 1):
         model.set_input(data)
         model.optimize_parameters()
 
-        if total_steps % opt.display_freq == 0:
-            visualizer.display_current_results(model.get_current_visuals(), epoch)
-
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
             visualizer.print_current_errors(epoch, epoch_iter, errors, t)
             if opt.display_id > 0:
-                visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
+                print(epoch, float(epoch_iter)/dataset_size)
 
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' %
